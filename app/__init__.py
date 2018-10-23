b5773259995319315010e86d79b5195f0bc4a118
jfcastro92 <jfcastro9208@gmail.com>
2018-10-23 10:57:44 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -27,18 +27,18 @@ app.config.update(
     DEBUG = True,
     TESTING = True,
     MONGODB_SETTINGS = {
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': 27017,
         'DB': 'weatherTasks',
         'TZ_AWARE': False,
     },
 )
 
-app.config['MQTT_BROKER_URL'] = 'localhost'  # use the free broker from HIVEMQ
-app.config['MQTT_BROKER_PORT'] = 1883  # default port for non-tls connection
-app.config['MQTT_USERNAME'] = ''  # set the username here if you need authentication for the broker
-app.config['MQTT_PASSWORD'] = ''  # set the password here if the broker demands authentication
-app.config['MQTT_KEEPALIVE'] = 5  # set the time interval for sending a ping to the broker to 5 seconds
+app.config['MQTT_BROKER_URL'] = '162.243.173.22'  # use the free broker from HIVEMQ
+app.config['MQTT_BROKER_PORT'] = 8883  # default port for non-tls connection
+app.config['MQTT_USERNAME'] = 'weathertasks'  # set the username here if you need authentication for the broker
+app.config['MQTT_PASSWORD'] = 'wtasks2018Admin'  # set the password here if the broker demands authentication
+app.config['MQTT_KEEPALIVE'] = 1  # set the time interval for sending a ping to the broker to 5 seconds
 app.config['MQTT_TLS_ENABLED'] = False  # set TLS to disabled for testing purposes
 
 
